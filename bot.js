a10fd41a7e1572a20d4225bbde445066cec25062
saransh484 <saransh8@hotmail.com>
2022-12-01 00:14:40 +0530

    initial commit

---
+++ bot.js
@@ -0,0 +1,18 @@
+const { Bot } = require("grammy");
+
+// Create an instance of the `Bot` class and pass your authentication token to it.
+const bot = new Bot("5832932056:AAFYVYicok5jLVw0lp_ku3vMaB7jOJxe_Wk"); // <-- put your authentication token between the ""
+
+// You can now register listeners on your bot object `bot`.
+// grammY will call the listeners when users send messages to your bot.
+
+// Handle the /start command.
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+// Handle other messages.
+bot.on("message", (ctx) => ctx.reply("RESPONSE"));
+
+// Now that you specified how to handle messages, you can start your bot.
+// This will connect to the Telegram servers and wait for messages.
+
+// Start the bot.
+bot.start();
